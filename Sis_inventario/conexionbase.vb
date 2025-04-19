675b0f6cba30a164f2a705facb86ea0edd6d007a
allancalero <78607262+allancalero@users.noreply.github.com>
2025-04-19 15:18:44 -0600

    first commit

---
+++ Sis_inventario/conexionbase.vb
@@ -0,0 +1,24 @@
+﻿Imports MySql.Data.MySqlClient
+Module conexionbase
+    Public conex As New MySqlConnection
+    Public ds As DataSet = New DataSet
+    Public da As MySqlDataAdapter
+    Public comando As MySqlCommand
+    Dim conectar As String
+
+    Sub conexion()
+        conectar = "server=localhost;user=root;password='2';database=inventario"
+        Try
+            conex.ConnectionString = conectar
+            conex.Open()
+            MsgBox("Conexion Sastifactoria")
+
+        Catch ex As Exception
+            MsgBox("Error en la contraseña" + ex.Message)
+
+        End Try
+    End Sub
+
+
+
+End Module
